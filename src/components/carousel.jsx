2b0805f7bad1bde24ff8caaf034956aef665f7cc
Ardana <ardana733@gmail.com>
2022-06-17 13:48:42 +0700

    add floating button

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -8,36 +8,36 @@ import image2 from "../assets/images/image2.jpg";
 import image3 from "../assets/images/image3.png";
 import imgBanner from "../assets/images/imgBanner.png";
 
-const responsive = {
-	0: {
-		items: 1,
-		stagePadding: 60,
-	},
-	600: {
-		items: 1,
-		stagePadding: 100,
-	},
-	1000: {
-		items: 1,
-		stagePadding: 200,
-	},
-	1200: {
-		items: 1,
-		stagePadding: 250,
-	},
-	1400: {
-		items: 1,
-		stagePadding: 300,
-	},
-	1600: {
-		items: 1,
-		stagePadding: 350,
-	},
-	1800: {
-		items: 1,
-		stagePadding: 400,
-	},
-};
+// const responsive = {
+// 	0: {
+// 		items: 1,
+// 		stagePadding: 60,
+// 	},
+// 	600: {
+// 		items: 1,
+// 		stagePadding: 100,
+// 	},
+// 	1000: {
+// 		items: 1,
+// 		stagePadding: 200,
+// 	},
+// 	1200: {
+// 		items: 1,
+// 		stagePadding: 250,
+// 	},
+// 	1400: {
+// 		items: 1,
+// 		stagePadding: 300,
+// 	},
+// 	1600: {
+// 		items: 1,
+// 		stagePadding: 350,
+// 	},
+// 	1800: {
+// 		items: 1,
+// 		stagePadding: 400,
+// 	},
+// };
 
 function carousel() {
 	return (
@@ -53,7 +53,7 @@ function carousel() {
 				// margin={5}
 				lazyLoad={true}
 				dots={false}
-				responsive={responsive}
+				// responsive={responsive}
 			>
 				<div className="item">
 					<div className="">
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import Navbar from "../components/navbar";
 import Carousel from "../components/carousel";
 import Products from "../components/products";
+import FloatingButton from "../components/floatingButton";
+
 
 export default function Home() {
 	return (<>
   <Navbar></Navbar>
   <Carousel></Carousel>
   <Products></Products>
+  <FloatingButton></FloatingButton>
   </>);
 }
