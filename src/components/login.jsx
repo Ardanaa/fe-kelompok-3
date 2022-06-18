a61f4977ddf6cbb25e2223a1815b8de501bb9c16
novivinop <nnovi452@gmail.com>
2022-06-18 12:48:24 +0700

    fix login

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,64 +1,99 @@
-import React from "react";
-import { Form, Col, Container, Row, Button } from "react-bootstrap";
+import { React, useRef, useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { Col, Container, Row, Form, Button, Alert } from "react-bootstrap";
+import axios from "axios";
 import { AiOutlineEye } from "react-icons/ai";
+import "../css/login.css";
+import imgLogin from "../assets/images/imageLogin.jpg";
 
 function Login() {
+  const navigate = useNavigate();
+
+	const emailField = useRef("");
+	const passwordField = useRef("");
+
+	const [errorResponse, setErrorResponse] = useState({
+		isError: false,
+		message: "",
+	});
+
+	const onRegister = async (e) => {
+		e.preventDefault();
+
+		try {
+			const userToRegisterPayload = {
+				email: emailField.current.value,
+				password: passwordField.current.value,
+			};
+
+			const registerRequest = await axios.post(
+				"http://localhost:2000/v1/auth/login",
+				userToRegisterPayload
+			);
+
+			const registerResponse = registerRequest.data;
+
+			if (registerResponse.status) navigate("/login");
+		} catch (err) {
+			console.log(err);
+			const response = err.response.data;
+
+			setErrorResponse({
+				isError: true,
+				message: response.message,
+			});
+		}
+	};
   return (
     <Container fluid className="ps-0">
       <Row>
         <Col className="pe-0 ">
-          <img src="/images/imgLogin.jpg" className="img-fluid bg" />
+          <img src={imgLogin} className="w-100" />
         </Col>
-        <Col className="mt-5 mx-auto">
-          <>
-            <div>
-              <p style={{ fontSize: "30px" }}>
-                <span style={{ fontWeight: "bold" }}>Masuk</span>
-              </p>
-            </div>
-            <Form>
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email</Form.Label>
-                <Form.Control
-                  type="email"
-                  placeholder="Contoh: johndee@gmail.com"
-                />
-              </Form.Group>
-              <div className="mb-3 box">
-                <input
-                  className="formBasicPassword"
-                  type="password"
-                  placeholder="Masukkan Password"
-                />
-                <button className="btn">
-                  <AiOutlineEye />
-                </button>
-              </div>
-              <Form className="mb-3 box" controlId="formBasicPassword">
-                <Form.Label>
-                  Password <AiOutlineEye />
-                </Form.Label>
-                <Form.Control type="password" placeholder="Masukkan password" />
-                <button className="btn">
-                  <AiOutlineEye />
-                </button>
-              </Form>
-              <Button
-                className="ms-auto button-primary radius-secondary bg-color-primary border-0"
-                variant="primary"
-                type="submit"
-              >
-                Submit
-              </Button>
-            </Form>
-            <p className="">
-              Belum punya akun?{" "}
-              <span className=" text-primary color-primary auto-layout daftar">
-                Daftar di sini
-              </span>
-            </p>
-          </>
-        </Col>
+        <Col className="ps-0">
+					<div className="center">
+						<Form onSubmit={onRegister}>
+							<h1 className="mb-3">Masuk</h1>
+							<Form.Group className="mb-3">
+								<Form.Label>Email</Form.Label>
+								<Form.Control
+									type="text"
+									ref={emailField}
+									placeholder="Contoh: johndee@gmail.com"
+									className="radius-primary"
+								/>
+							</Form.Group>
+							<Form.Group className="mb-3">
+								<Form.Label>Password</Form.Label>
+								<Form.Control
+									type="password"
+									ref={passwordField}
+									placeholder="Masukkan Password"
+									className="radius-primary"
+								/>
+							</Form.Group>
+							{errorResponse.isError && (
+								<Alert variant="danger">{errorResponse.message}</Alert>
+							)}
+							<Button
+								className="w-100 radius-primary bg-color-secondary"
+								type="submit"
+							>
+								Masuk
+							</Button>
+							<p className="m-4 text-center">
+								Belum punya akun?{" "}
+								<Link
+									to="/register"
+									className="text-decoration-none"
+									style={{ color: "#7126B5" }}
+								>
+									Daftar di sini
+								</Link>
+							</p>
+						</Form>
+					</div>
+				</Col>
       </Row>
     </Container>
   );
