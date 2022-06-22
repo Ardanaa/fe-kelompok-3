7e4e88dbc8b750a95aee5dc0833500193ee1498e
Ardana <ardana733@gmail.com>
2022-06-22 21:09:10 +0700

    mapping with dummy api

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -1,12 +1,27 @@
-import { React } from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Button, Row, Col, Card } from "react-bootstrap";
 import { FiSearch } from "react-icons/fi";
+import axios from "axios";
 import jam1 from "../assets/images/jam1.png";
 import jam2 from "../assets/images/jam2.png";
 import "../css/products.css"
 // import Axios from "axios";
 
-function product() {
+function Product() {
+	const [post, setPost] = useState([]);
+
+	useEffect(() => {
+		const postData = async () => {
+			const response = await axios.get(`https://fakestoreapi.com/products`);
+			console.log(response);
+			const data = await response.data;
+			console.log(data);
+
+			setPost(data);
+		};
+		postData();
+	}, []);
+
 	return (
 		<Container className="pt-5" id="btn-category">
 			<h5 className="fw-bold">Telusuri Kategori</h5>
@@ -38,17 +53,19 @@ function product() {
 			</div>
 			<Container className="mt-5">
 				<Row md={6}>
-					<Col>
-						<Card >
-							<Card.Img variant="top" className="p-1" src={jam1} />
-							<Card.Body>
-								<Card.Title className="fs-7">Jam Tangan Casio</Card.Title>
-								<p className="text-black-50">Aksesoris</p>
-								<Card.Text>Rp. 250.000</Card.Text>
-							</Card.Body>
-						</Card>
-					</Col>
-					<Col>
+					{post.map((post) =>
+						<Col key={post.id}>
+							<Card >
+								<Card.Img variant="top" className="p-2" src={post.image} style={{ maxHeight: "100px", objectFit: "cover" }} />
+								<Card.Body>
+									<Card.Title className="fs-7">{post.title}</Card.Title>
+									<p className="text-black-50">{post.category}</p>
+									<Card.Text>{post.price}</Card.Text>
+								</Card.Body>
+							</Card>
+						</Col>
+					)}
+					{/* <Col>
 						<Card >
 							<Card.Img variant="top" className="p-1" src={jam2} />
 							<Card.Body>
@@ -159,11 +176,11 @@ function product() {
 								<Card.Text>Rp. 250.000</Card.Text>
 							</Card.Body>
 						</Card>
-					</Col>
+					</Col> */}
 				</Row>
 			</Container>
 		</Container>
 	);
 }
 
-export default product;
+export default Product;
